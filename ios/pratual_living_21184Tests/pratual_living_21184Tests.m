f9212b2aa6f7eaae53b2294305f9680f669a6f2a
Crowdbotics <team@crowdbotics.com>
2020-10-06 19:38:23 +0000

    Initial commit.

---
+++ ios/pratual_living_21184Tests/pratual_living_21184Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface pratual_living_21184 : XCTestCase
+
+@end
+
+@implementation pratual_living_21184
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
